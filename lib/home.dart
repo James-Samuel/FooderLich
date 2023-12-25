43894b37d4e9ef3104f8e0488fbf4847217b0fd2
James Samuel Dunstan <jay_sam1@hotmail.com>
2023-12-25 21:06:34 +0300

    Mon25Dec_21:06

--- lib/home.dart
+++ lib/home.dart
@@ -1,1 +1,60 @@
-import 'package:flutter/material.dart';// 1class Home extends StatefulWidget {  const Home({Key? key}) : super(key: key);  @override  _HomeState createState() => _HomeState();}class _HomeState extends State<Home> {  // TODO: Add state variables and functions  @override  Widget build(BuildContext context) {    return Scaffold(      appBar: AppBar(        title: Text(          'Fooderlich',          // 2          style: Theme.of(context).textTheme.headline6,        ),      ),      // TODO: Show selected tab      body: Center(          child: Text('Let\'s get cooking ! ',// 3              style: Theme.of(context).textTheme.headline1)),      // TODO: Add bottom navigation bar    );  } }
+import 'package:flutter/material.dart';
+
+// 1
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  // TODO: Add state variables and functions
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'Fooderlich',
+            // 2
+            // ignore: deprecated_member_use
+            style: Theme.of(context).textTheme.headline6,
+          ),
+        ),
+      ),
+      // TODO: Show selected tab
+      body: Center(
+        child: Center(
+          child: Text('Let\'s get cooking ! ',
+              // 3
+              // ignore: deprecated_member_use
+              style: Theme.of(context).textTheme.headline1),
+        ),
+      ),
+
+      // ********************** NAVIGATION BAR *******
+      // TODO: Add bottom navigation bar
+      //navigation bar
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        // TODO: Set selected tab bar
+        // 6
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card2',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card3',
+          ),
+        ],
+      ),
+      // ********************** NAVIGATION BAR *******
+    );
+  }
+}
